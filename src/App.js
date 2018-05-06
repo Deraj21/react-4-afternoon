7fd33e5b4bafd75f24a9de4b5ebcbfae1e3d2c2a
deraj21 <deraj21@gmail.com>
2018-05-05 17:12:44 -0700

    Seet Everything up, and finished step 2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import routes from './routes.js';
 
 export default class App extends Component {
   render() {
@@ -11,6 +12,9 @@ export default class App extends Component {
               <div className='links'>About</div> 
           </div>
         </nav>
+
+        { routes }
+
       </div>
     )
   }
